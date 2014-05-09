0a910357b5cde03e0d9efdd5932fdc34f76c7b2c
@yepesasecas <yepes07@gmail.com>
2014-05-09 16:39:57 -0400

    add feature Plane.near

--- plane.rb
+++ plane.rb
@@ -1,7 +1,8 @@
 class Plane
-  attr_accessor :nodes
+  attr_accessor :nodes, :nodes_distances
   def initialize()
     @nodes = []
+    @nodes_distances = []
   end  
 
   def add_node(node)
@@ -9,11 +10,34 @@ class Plane
   end
 
   def distances
-    for i in 0..nodes.count-1 do
-      for j in i..nodes.count-1 do
-        dist = Distance.new from: nodes[i], to: nodes[j]
+    for i in 0..count do
+      for j in i..count do
+        dist = distance(nodes[i], nodes[j])
         puts "#{i} - #{j}: #{dist.distance}"
       end
     end
   end
+
+  def near(node, radius)
+    nears = []
+    for i in 0..count do
+      iNode = nodes[i]
+      dist = distance(node, iNode).distance
+      if node != iNode && dist < radius
+        nears.push({node: iNode, distance: dist})
+      end
+    end
+    nears
+  end
+
+  def distance(nodeA, nodeB)
+     dist = Distance.new from: nodeA, to: nodeB
+     dist
+  end
+
+  private
+    def count
+      count ||= nodes.count - 1
+      count
+    end
 end
--- test.rb
+++ test.rb
@@ -1,19 +1,9 @@
 load "plane.rb"
 load "distance.rb"
 load "node.rb"
+load "plane_factory.rb"
 
-plane = Plane.new
-nodeA = Node.new [1,1]
-nodeB = Node.new [2,3]
-nodeC = Node.new [4,1]
-nodeD = Node.new [5,4]
-nodeE = Node.new [7,12]
+plane = PlaneFactory.create
 
-
-plane.add_node nodeA
-plane.add_node nodeB
-plane.add_node nodeC
-plane.add_node nodeD
-plane.add_node nodeE
-
-puts plane.distances
+# p plane.distances
+p plane.near(plane.nodes.first, 4).count
