7c49bf134b0f9c429f3b9d02ae19ea77228ea42e
@yepesasecas <yepes07@gmail.com>
2014-05-09 16:40:41 -0400

    add PlaneFactory

---
+++ plane_factory.rb
@@ -0,0 +1,19 @@
+module PlaneFactory
+  def self.create
+    plane = create_nodes(Plane.new)
+    plane
+  end
+
+  def self.create_nodes(plane)
+    1000.times do
+      coordinates = [sample, sample,sample]
+      node        = Node.new(coordinates)
+      plane.add_node(node)
+    end
+    plane
+  end
+
+  def self.sample
+    (0..10).to_a.sample
+  end
+end
